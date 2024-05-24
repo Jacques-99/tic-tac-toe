e481737687b78916b35c174199bdfacf01ef76a8
Jacques <83700389+Jacques-99@users.noreply.github.com>
2024-05-24 22:43:21 +0200

    added logs and mvp

---
+++ ReadMe.md
@@ -0,0 +1,33 @@
+# Tic-Tac-Toe
+
+This is a basic tic-tac-toe game.
+
+## Installation
+
+There are 2 methods to install the required packages for the project:
+
+### 1. Requirements.txt:
+
+```
+pip install -r requirements.txt
+```
+### 2. Poetry:
+
+```
+pip install poetry
+poetry shell
+poetry install
+```
+
+## Run the application:
+
+Simply run the following command:
+```
+python main.py
+```
+## Run the unit tests
+To run the unit tests to ensure the functions work as expected run the following command:
+
+```
+
+```
--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 # Example file showing a circle moving on screen
 import pygame
 import sys
+import logging
+import numpy as np
+import time
 
 class Game:
     def __init__(self) -> None:
+        # Set up logging
+        logging.basicConfig(
+            filename='logs.log', 
+            level=logging.INFO,
+            format="%(asctime)s:%(filename)s - %(funcName)s:%(lineno)s - %(message)s",
+            )
+        
+        logging.info('------ New Game Started ------')
+
         # pygame setup
         pygame.init()
         pygame.display.set_caption("Noice")
@@ -11,7 +23,6 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
         self.dt = 0
-        self.player_pos = pygame.Vector2(self.screen.get_width() / 2, self.screen.get_height() / 2)
 
         #=== Screen ===#
         self.screen_width = self.screen.get_width()
@@ -20,12 +31,18 @@ class Game:
         self.col = [0,self.screen_width/3,self.screen_width*2/3]
         self.row = [0,self.screen_height/3,self.screen_height/3*2]
 
+        #=== Text ===#
+        self.font = pygame.font.Font('freesansbold.ttf',32)
+        self.text = self.font.render('', True, 'green')
+        self.textRect = self.text.get_rect()
+        self.textRect.center = (self.screen_width / 2.8, self.screen_height / 2)
+
         #=== Grid ===#
         self.grid_scale = pygame.Vector2(self.screen.get_width(), self.screen.get_height())
-        self.grid_img = pygame.image.load("images/grid2.png").convert_alpha()
+        self.grid_img = pygame.image.load("images/grid.png").convert_alpha()
         self.grid = pygame.transform.scale(self.grid_img, self.grid_scale)
 
-        self.matrix = [[2,2,2],[2,2,2],[2,2,2]]
+        self.matrix = [[None,None,None],[None,None,None],[None,None,None]]
 
         #=== Cross ===#
         self.cross_scale = pygame.Vector2(self.screen.get_width()/3, self.screen.get_height()/3)
@@ -41,74 +58,193 @@ class Game:
         self.player1 = 0
         self.player2 = 1
         self.current_player = self.player1
+        self.next_player = self.player1       
 
     def run(self):
+
+        #=== Functions ===#
+
+        def has_winning_list(ls:list):
+            try:
+                if sum(ls) == self.current_player*3:
+                    return True
+            except TypeError as e:
+                pass
+            return False
+        
+        def determine_block(pos):
+            if pos > self.col[2]:
+                return 2 # column 3
+            elif pos < self.col[1]: 
+                return 0 # column 1
+            else:
+                return 1 # column 2
+        
+        def is_victorious():
+            # Check if the current player has a winning row:
+            for i, row in enumerate(self.matrix):
+                if has_winning_list(row):
+                    logging.info(f'Player {self.current_player} won in row {i}')
+                    return [True,f"row{i+1}"]
+            
+            # Check if the current player has a winning col:
+            transposed_matrix = np.transpose(np.array(self.matrix)).tolist()
+
+            for j, col in enumerate(transposed_matrix):
+                if has_winning_list(col):
+                    logging.info(f'Player {self.current_player} won in col {j}')
+                    return [True,f"col{j+1}"]
+
+            # Check if the current player has a winning diagonal:
+            diagonal1=[self.matrix[0][0],self.matrix[1][1],self.matrix[2][2]]
+            if has_winning_list(diagonal1):
+                logging.info(f'Player {self.current_player} won in diagonal 1')
+                return [True,"diagonal1"]
+
+            diagonal2 = [self.matrix[2][0],self.matrix[1][1],self.matrix[0][2]]
+            if has_winning_list(diagonal2):
+                logging.info(f'Player {self.current_player} won in diagonal 2')
+                return [True,"diagonal2"]
+            
+            return [False,None]
+
+        def take_turn(player):
+            # Get the mouse position where the user clicked:
+            mouse_pos = pygame.mouse.get_pos()
+            # Determine x and y coordinates of the mouse:
+            x_pos = mouse_pos[0]
+            y_pos = mouse_pos[1]
+            # Determine the row and column the user clicked:
+            column = determine_block(x_pos)
+            row = determine_block(y_pos)
+            
+            # Determine if the block was already selected:
+            if self.matrix[row][column] != None:
+                illigal_move_message = self.font.render(f'illegal move!', True, 'red')
+                draw_board()
+                self.screen.blit(illigal_move_message,self.textRect)
+                logging.info(f'Player {self.current_player+1} attepted to make an illigal move on row {row+1} and col {column+1}')
+                pygame.display.update()
+                time.sleep(0.8)
+            else:
+                # Update the matrix to display the relevent icon:
+                self.matrix[row][column] = player
+                logging.info(f'Player {self.current_player+1} played at row {row+1} and {column+1}')
+                # Switch the turn of the player:
+                self.next_player = 1-self.current_player
+        
+        def draw_board():
+            for i, row in enumerate(self.matrix):
+                for j, col in enumerate(row):
+                    try:
+                        if col == 1:
+                            self.screen.blit(self.cross, (self.col[j],self.row[i]))
+                        elif col == 0:
+                            self.screen.blit(self.circle, (self.col[j],self.row[i]))
+                        elif col != None:
+                            raise Exception(f"Unexpected value in board at position ({i}, {j})")
+                    except Exception as e:
+                        logging.error(str(e))
+                        raise
+
+        def game_is_draw():
+            game_completed = True
+            for row in self.matrix:
+                for col in row:
+                    if col == None:
+                        game_completed = False
+                        return False
+            return game_completed
+        
+        def end_game():
+            logging.info('------ Game Ended ------')
+
+            # Delay before exiting the game:
+            time.sleep(3)
+            pygame.quit()
+            sys.exit()    
+
+        def draw_line(win_row):
+            a11=(self.screen_width // 6, self.screen_height // 6)
+            a12=(self.screen_width // 2, self.screen_height // 6)
+            a13=((self.screen_width // 6)*5,self.screen_width // 6)
+            a21=(self.screen_width // 6, self.screen_height // 2)
+            a23=((self.screen_width // 6)*5, self.screen_height // 2)
+            a31=(self.screen_width // 6,(self.screen_width // 6)*5)
+            a32=(self.screen_width // 2, (self.screen_width // 6)*5)
+            a33=((self.screen_width // 6)*5, (self.screen_height // 6)*5)
+            
+            blue_colour = (30, 70, 150)
+
+            if win_row == "row1":
+                pygame.draw.line(self.screen, blue_colour, a11, a13 , 20)
+            elif win_row == "row2":
+                pygame.draw.line(self.screen, blue_colour, a21, a23 , 20)
+            elif win_row == "row3":
+                pygame.draw.line(self.screen, blue_colour, a31, a33 , 20)
+            elif win_row == "col1":
+                pygame.draw.line(self.screen, blue_colour, a11, a31 , 20)
+            elif win_row == "col2":
+                pygame.draw.line(self.screen, blue_colour, a12, a32 , 20)
+            elif win_row == "col3":
+                pygame.draw.line(self.screen, blue_colour, a13, a33 , 20)
+            elif win_row == "diagonal1":
+                pygame.draw.line(self.screen, blue_colour, a11, a33 , 20)
+            elif win_row == "diagonal2":
+                pygame.draw.line(self.screen, blue_colour, a31, a13 , 20)
+            else:
+                raise Exception(f"Unexpected value. Cannot draw line: {win_row}")
+            
         while True:
-            
+            # Switch the turn of the player:
+            self.current_player = self.next_player
+
             # fill the screen with a color to wipe away anything from last frame
-            self.screen.fill("black")
+            self.screen.fill("white")
             
+            # Print the grid:
             self.screen.blit(self.grid, (0,0))
 
-            # self.matrix = [[1,0,1],[0,1,1],[1,0,0]]
-
-            for row in range(len(self.matrix)):
-                for col in range(len(self.matrix[row])):
-                    if self.matrix[row][col] == 1:
-                        self.screen.blit(self.cross, (self.col[col],self.row[row]))
-                    elif self.matrix[row][col] == 0:
-                        self.screen.blit(self.circle, (self.col[col],self.row[row]))
-                    else: 
-                        pass
-
-            def determine_pos(pos):
-                if pos > self.col[2]:
-                    return 2 # column 3
-                elif pos < self.col[1]: 
-                    return 0 # column 1
-                else:
-                    return 1 # column 2
-
-            # poll for events
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:   # pygame.QUIT event means the user clicked X to close your window
-                    pygame.quit()
-                    sys.exit()
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    mouse_pos = pygame.mouse.get_pos()
-                    print(f"Player {self.current_player} clicked mouse at {mouse_pos}")
+                if event.type == pygame.QUIT:              # pygame.QUIT event means the user clicked X to close the window
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN: # Check if the player has clicked the mouse
+                    take_turn(self.current_player)
+                    break
 
-                    x_pos = mouse_pos[0]
-                    print(x_pos)
-                    y_pos = mouse_pos[1]
-                    print(y_pos)
-                    column = determine_pos(x_pos)
-                    row = determine_pos(y_pos)
+            # Draw the circles and crosses of the current board:
+            draw_board()
+
+            # Determine if the current player won
+            results=is_victorious()
+            if results[0] == True:
+                    # Generate and display a win message:
+                    win_message = self.font.render(f'Player {self.current_player + 1} Won', True, 'green')
                     
-                    self.matrix[row][column] = self.current_player
-                    print(self.matrix)
-                    self.current_player = 1-self.current_player
+                    try:
+                        draw_line(results[1])
+                    except Exception as e:
+                        logging.error(str(e))
+                        raise
 
-            
-            # check if current player has won the game:
-                # find cross product of matrix with [1,1,1]
-                    # if the result contains a 3 it means player2 won with a row
-                    # if the result contains a 0 it means player1 won with a row
-                
-                # find the transpose of the matrix
-                    # if the result contains a 3 it means player2 won with a column
-                    # if the result contains a 0 it means player1 won with a column
+                    self.screen.blit(win_message,self.textRect)
+                    pygame.display.flip()
 
-                # check if there's a winning combo in one of the diagonals
+                    end_game()          
 
+            # Determine if the game ended in a draw:
+            elif game_is_draw():
+                    # Generate and display a draw message:
+                    draw_message = self.font.render(f'No winner', True, 'orange')
+                    self.screen.blit(draw_message,self.textRect)
+                    logging.info('Game ended in a draw')
+                    pygame.display.flip()
+                    end_game()
 
-            # flip() the display to put your work on screen
+            # Display the board and icons:
             pygame.display.flip()
-            pygame.display.update()
 
-            # limits FPS to 60
-            # dt is delta time in seconds since last frame, used for framerate-
-            # independent physics.
-            self.dt = self.clock.tick(60) / 1000         
-
+            # Increase the delta time since the last frame:
+            self.dt = self.clock.tick(60) / 1000 
+                    
 Game().run()
